e0e417ee5742b3e8dca482301f3a66be45092ce7
earx <eascarrunz.dev@mailfence.com>
2025-04-22 15:16:09 +0200

    slot for only one parent in FeatureNode

--- bin/stats/feature_nodes.py
+++ bin/stats/feature_nodes.py
@@ -1,34 +1,40 @@
 import numpy as np
 from dataclasses import dataclass, field
-from typing import Any, List, Generator
+from typing import Optional, List, Generator
 
 
 @dataclass(slots=True)
 class FeatureNode:
-    parents: List["FeatureNode"] = field(default_factory=list)
+    parents: Optional["FeatureNode"]
     children: List["FeatureNode"] = field(default_factory=list)
-    identifier: str = ""
-    region: str = ""
+    region: str = ""            # Genomic region from GFF file
+    identifier: str = ""        # Identifier string from the GFF file
+    biotype: str = ""
+    dataindex: np.int32 = -1    # Row index in external data table(s)
     start: np.int64 = -1
     end: np.int64 = -1
-    data: dict[str, Any] = field(default_factory=dict[str, Any])
 
     def add(self, child: "FeatureNode") -> None:
         """Add a child node"""
+        if self == child:
+            raise Exception(
+                "Attempted to link node \"{self.identifier}\" to itself"
+            )
+
         if self.region != child.region:
             raise Exception(
                 f"Tried to add node \"{child.identifier}\" in genomic region \"{child.region}\" as a child of node \"{self.identifier}\" in genomic region \"{self.region}\""
             )
 
         self.children.append(child)
-        child.parents.append(self)
+        child.parent = self
         self.start = child.start if child.start < self.start else self.start
         self.end = child.end if child.end > self.end else self.end
 
         return None
 
     def is_root(self) -> bool:
-        """Return true if the node is a root (has not parents)"""
+        """Return true if the node is a root (it has no parent)"""
 
         return len(self.parents) == 0
 
