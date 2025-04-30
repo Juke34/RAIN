62d8225ee8007391cd8cffc87aa819e02c1d9d54
earx <eascarrunz.dev@mailfence.com>
2025-04-30 17:09:51 +0200

    Draft of feature-wise edition counting scripts

--- bin/stats/utils.py
+++ bin/stats/utils.py
@@ -1,13 +1,10 @@
-import Bio
 import numpy as np
-from typing import Generator
-from enum import IntEnum
+import enum
 from numpy.typing import NDArray
 from dataclasses import dataclass
 from frozendict import frozendict
 
-
-class NucCode(IntEnum):
+class NucCode(enum.IntEnum):
     A = 0
     C = 1
     G = 2
@@ -25,28 +22,4 @@ class SiteVariantData:
     strand: np.int32
     coverage: np.float32
     mean_quality: np.float32
-    frequencies: NDArray[np.float32]
-
-
-def group_by_overlap(
-    record: Bio.SeqRecord,
-) -> Generator[list[Bio.SeqFeature], None, None]:
-    """Create an iterator that yields groups of features with overlapping genomic positions within a record"""
-
-    feature: Bio.SeqFeature = record.features[0]
-    start: Bio.SeqFeature.ExactPosition = feature.location.start
-    end: Bio.SeqFeature.ExactPosition = feature.location.end
-
-    group: list[Bio.SeqFeature] = []
-
-    for feature in record.features:
-        if start <= feature.location.start <= end:
-            end = feature.location.end
-            group.append(feature)
-        else:
-            yield group
-            start = feature.location.start
-            end = feature.location.end
-            group = [feature]
-
-    yield group
+    frequencies: NDArray[np.int32]
